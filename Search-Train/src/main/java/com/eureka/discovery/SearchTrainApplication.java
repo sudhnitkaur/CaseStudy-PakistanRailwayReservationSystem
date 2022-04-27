53bbfc899fdf3d7215b6ed2189a7024daef9b742
harsh-vardhan-98 <harshkaurav37@gmail.com>
2022-04-27 22:59:13 +0530

    repository link added

---
+++ Search-Train/src/main/java/com/eureka/discovery/SearchTrainApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchTrainApplication.class, args);
+	}
+
+}
